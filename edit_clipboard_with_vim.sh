af4386f96d188b34357b3c472790fe537903525e
Falko Strenzke <{ID}+{username}@users.noreply.github.com>
2022-06-10 11:03:41 +0200

    added files

---
+++ edit_clipboard_with_vim.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+path_prefix="/tmp/vim_clip_edit_"
+if compgen -G "$path_prefix*" > /dev/null; then
+    rm $tmp_file
+    gnome-terminal --wait -- /bin/sh -c "echo \"tmp file pattern exists! another instance may be running. Otherwise, delete stale files with prefix $path_prefix\"; press_any_key_to_cont.sh"
+    exit 1
+fi
+now=$(date +%s)
+tmp_file="$path_prefix$now.txt"
+xclip -o > $tmp_file 
+gnome-terminal --wait -- vim $tmp_file
+cat $tmp_file | xclip -i
+rm $tmp_file
